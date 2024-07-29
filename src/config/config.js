9e39d012eb3ee43f62b6f497af0552d25d176afb
“treyp99” <nwachukwupgate99@gmail.com>
2024-07-29 21:33:35 +0100

    Fixed case error

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 // navbar links
 export const links = [
-    { href: "/create", text: "Create Recipe" },
+    { href: "/make", text: "Create Recipe" },
 //     { href: "/browse-topics", text: "Browse Topics" },
 //     { href: "/random-item", text: "Random Item" },
 //     { href: "/experts", text: "Experts" },
